7b92e8e3a88784b2d90394f863f0636265d8954c
Wajdi Jurry <creations.forum@gmail.com>
2023-11-16 14:29:42 +0200

    Fix currency session

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Wjurry\RedParts\Http\Controllers\ConfigController;
 use Wjurry\RedParts\Http\Controllers\CurrencyController;
 
-Route::get('/config', [ConfigController::class, 'index']);
-Route::post('/currency/switch/{currency}', [CurrencyController::class, 'switch']);
+Route::middleware('web')->group(function () {
+    Route::get('/config', [ConfigController::class, 'index']);
+    Route::post('/currency/switch/{currency}', [CurrencyController::class, 'switch']);
+});
