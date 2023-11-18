c723460a3556993ad258c30bd8f10755227d631b
Wajdi Jurry <creations.forum@gmail.com>
2023-11-18 11:07:24 +0200

    Add MenuSeeder

--- src/database/seeders/MenuSeeder.php
+++ src/database/seeders/MenuSeeder.php
@@ -9,11 +9,13 @@ use Outl1ne\MenuBuilder\Models\MenuItem;
 
 class MenuSeeder extends Seeder
 {
-    private $callUsMenuId;
+    private int $callUsMenuId;
 
-    private $topRightMenuId;
+    private int $topRightMenuId;
 
-    protected $topLeftMenuId;
+    private int $topLeftMenuId;
+
+    private int $mainMenuId;
 
     public function run(): void
     {
@@ -45,6 +47,14 @@ class MenuSeeder extends Seeder
             $topLeftMenu = (new Menu(['name' => 'TopLeft', 'slug' => 'red-parts']));
             $topLeftMenu->save();
             $this->topLeftMenuId = $topLeftMenu->id;
+        }
+
+        try {
+            Menu::query()->where('name', 'MainMenu')->firstOrFail();
+        } catch (ModelNotFoundException $exception) {
+            $mainMenu = (new Menu( ["name" => 'MainMenu', 'slug' => 'red-parts']));
+            $mainMenu->save();
+            $this->mainMenuId = $mainMenu->id;
         }
     }
 
@@ -346,6 +356,331 @@ class MenuSeeder extends Seeder
                 ],
                 "parent_id" => null,
                 "order" => 20,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+        }
+
+        if ($this->mainMenuId) {
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Home",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => null,
+                "order" => 1,
+                "enabled" => 1,
+                "nestable" => 1
+            ]));
+
+            $shopMenuItemEn = new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Shop",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/shop",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => null,
+                "order" => 2,
+                "enabled" => 1,
+                "nestable" => 1
+            ]);
+            $shopMenuItemEn->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Cart",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/cart",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $shopMenuItemEn->id,
+                "order" => 1,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Checkout",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/checkout",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $shopMenuItemEn->id,
+                "order" => 2,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Track Order",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/track-order",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $shopMenuItemEn->id,
+                "order" => 3,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            $accountMenuEn = new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Account",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/account",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => null,
+                "order" => 3,
+                "enabled" => 1,
+                "nestable" => 1
+            ]);
+            $accountMenuEn->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Garage",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/garage",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $accountMenuEn->id,
+                "order" => 1,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Orders",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/my-orders",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $accountMenuEn->id,
+                "order" => 2,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Addresses",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/addresses",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $accountMenuEn->id,
+                "order" => 3,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Account Details",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/account",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => $accountMenuEn->id,
+                "order" => 4,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "Could not find your part?",
+                "locale" => "en",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/contact",
+                "target" => "_self",
+                "data" => null,
+                "parent_id" => null,
+                "order" => 31,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "الرئيسية",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => null,
+                "order" => 32,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            $shopMenuItemAr = new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "المتجر",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/shop",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => null,
+                "order" => 33,
+                "enabled" => 1,
+                "nestable" => 1
+            ]);
+            $shopMenuItemAr->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "سلة الشراء",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/cart",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $shopMenuItemAr->id,
+                "order" => 34,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "صفحة الدفع",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/checkout",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $shopMenuItemAr->id,
+                "order" => 35,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "تتبع الطلب",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/track-order",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $shopMenuItemAr->id,
+                "order" => 36,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            $accountMenuAr = new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "حسابي",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/account",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => null,
+                "order" => 37,
+                "enabled" => 1,
+                "nestable" => 1
+            ]);
+            $accountMenuAr->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "كراج السيارات",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/garage",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $accountMenuAr->id,
+                "order" => 38,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "طلباتي",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/my-orders",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $accountMenuAr->id,
+                "order" => 39,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "عناوين التوصيل",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/addresses",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $accountMenuAr->id,
+                "order" => 40,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "معلومات الحساب",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/account",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => $accountMenuAr->id,
+                "order" => 41,
+                "enabled" => 1,
+                "nestable" => 1
+            ]))->save();
+
+            (new MenuItem([
+                "menu_id" => $this->mainMenuId,
+                "name" => "لم تعثر على ما تبحث عنه؟",
+                "locale" => "ar",
+                "class" => "Outl1ne\MenuBuilder\MenuItemTypes\MenuItemStaticURLType",
+                "value" => "/contact",
+                "target" => "_self",
+                "data" => [
+                ],
+                "parent_id" => null,
+                "order" => 42,
                 "enabled" => 1,
                 "nestable" => 1
             ]))->save();
